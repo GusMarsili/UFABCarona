bf444010847d5c1ddb514f8730ff46cc1b90ac00
Guilherme Eras <135174494+GuilhermeRodriguesEras@users.noreply.github.com>
2025-04-17 12:28:46 -0300

    Adicionando filtro de pesquisa na pagina Uber

--- ufabcarona/lib/pages/uber_page.dart
+++ ufabcarona/lib/pages/uber_page.dart
@@ -4,40 +4,40 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class UberPage extends StatelessWidget {
+class UberPage extends StatefulWidget {
   final User user;
+  final bool mostrarFiltros = false;
   const UberPage({super.key, required this.user});
 
-  // Função para deletar um grupo Uber com confirmação
-  // Future<void> _deleteUberGroup(
-  //   DocumentSnapshot document,
-  //   BuildContext context,
-  // ) async {
-  //   bool confirm = await showDialog(
-  //     context: context,
-  //     builder:
-  //         (ctx) => AlertDialog(
-  //           title: const Text("Confirmar Deleção"),
-  //           content: const Text("Deseja deletar este grupo Uber?"),
-  //           actions: [
-  //             TextButton(
-  //               child: const Text("Cancelar"),
-  //               onPressed: () => Navigator.of(ctx).pop(false),
-  //             ),
-  //             TextButton(
-  //               child: const Text("Deletar"),
-  //               onPressed: () => Navigator.of(ctx).pop(true),
-  //             ),
-  //           ],
-  //         ),
-  //   );
-  //   if (confirm) {
-  //     await document.reference.delete();
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(content: Text("Grupo Uber deletado com sucesso!")),
-  //     );
-  //   }
-  // }
+  @override
+  State<UberPage> createState() => _UberPageState();
+}
+
+class _UberPageState extends State<UberPage> {
+  late final User user;
+  late bool mostrarFiltros;
+  final TextEditingController _controllerOrigem = TextEditingController();
+  final TextEditingController _controllerDestino = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    user = widget.user;
+    mostrarFiltros = widget.mostrarFiltros;
+  }
+
+  @override
+  void dispose() {
+    _controllerOrigem.removeListener(_filterRides);
+    _controllerDestino.removeListener(_filterRides);
+    super.dispose();
+  }
+
+  void _filterRides() {
+    setState(() {
+      // Chama o setState para atualizar a UI sempre que os filtros mudarem
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,33 +57,82 @@ class UberPage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.filter_alt),
+            onPressed: () {
+              setState(() {
+                mostrarFiltros = !mostrarFiltros;
+              });
+            },
+          ),
+        ],
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: uberGroups.orderBy('timestamp', descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Center(
-              child: Text(
-                "Nenhum grupo Uber encontrado.",
-                style: TextStyle(color: Colors.black),
-              ),
-            );
-          }
-          return ListView(
-            children:
-                snapshot.data!.docs.map((DocumentSnapshot document) {
-                  Map<String, dynamic> data =
-                      document.data() as Map<String, dynamic>;
-                  // Verifica se o grupo foi criado pelo usuário atual
-                  bool isOwner = data['creatorId'] == user.uid;
-                  return UberCard(data: data).build(isOwner, user, document.id, context);
-                }).toList(),
-          );
-        },
+      body: Column(
+        children: [
+          if (mostrarFiltros)
+            Column(
+              children: [
+                SizedBox(height: 5),
+                TextFieldElement(
+                  labelText: "Origem",
+                  showSearchIcon: true,
+                  haveController: true,
+                  controller: _controllerOrigem,
+                  onChanged: (_) => _filterRides(),
+                ).build(context),
+                SizedBox(height: 7),
+                TextFieldElement(
+                  labelText: "Destino",
+                  showSearchIcon: true,
+                  haveController: true,
+                  controller: _controllerDestino,
+                  onChanged: (_) => _filterRides(),
+                ).build(context),
+              ],
+            ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  uberGroups.orderBy('timestamp', descending: true).snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      "Nenhum grupo Uber encontrado.",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  );
+                }
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                        Map<String, dynamic> data =
+                            document.data() as Map<String, dynamic>;
+                        // Verifica se a carona foi criada pelo usuário atual
+                        if (data['origem'].toLowerCase().contains(
+                              _controllerOrigem.text.toLowerCase(),
+                            ) &&
+                            data['destino'].toLowerCase().contains(
+                              _controllerDestino.text.toLowerCase(),
+                            )) {
+                          bool isOwner = data['creatorId'] == user.uid;
+                          return UberCard(
+                            data: data,
+                          ).build(isOwner, user, document.id, context);
+                        } else {
+                          return Container();
+                        }
+                      }).toList(),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
