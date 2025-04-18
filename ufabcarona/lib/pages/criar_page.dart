2da1d9fd35a6c5fb9b60dec64b6974c2a9852f71
Vitória Santos <91280785+vitasantos@users.noreply.github.com>
2025-04-17 22:19:01 -0300

    Update criar_page.dart

--- ufabcarona/lib/pages/criar_page.dart
+++ ufabcarona/lib/pages/criar_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ufabcarona/pages/elements_imports.dart';
 import 'carona_forms.dart';
 import 'uber_forms.dart';
 
