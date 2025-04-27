64c0b117a8fb3bdf9fa1834606fd53d9ba6c0dc4
Guilherme Eras <135174494+GuilhermeRodriguesEras@users.noreply.github.com>
2025-04-27 15:44:26 -0300

    Arrumando o erro do filtro na pagina uber

--- ufabcarona/lib/pages/uber_page.dart
+++ ufabcarona/lib/pages/uber_page.dart
@@ -57,82 +57,80 @@ class _UberPageState extends State<UberPage> {
             fontWeight: FontWeight.bold,
           ),
         ),
-         // actions: [
-        //   IconButton(
-        //     icon: Icon(Icons.filter_alt),
-        //     onPressed: () {
-        //       setState(() {
-        //         mostrarFiltros = !mostrarFiltros;
-        //       });
-        //     },
-        //   ),
-        // ],
+        /*actions: [
+          IconButton(
+            icon: Icon(Icons.filter_alt),
+            onPressed: () {
+              setState(() {
+                mostrarFiltros = !mostrarFiltros;
+              });
+            },
+          ),
+        ],*/
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: uberGroups.orderBy('updatedAt', descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Center(
-              child: Text(
-                "Nenhum grupo Uber encontrado.",
-                style: TextStyle(color: Colors.black),
-              ),
-            );
-          }
-          return ListView(
-            children: [
-              if (mostrarFiltros)
-                Column(
-                  children: [
-                    SizedBox(height: 5),
-                    TextFieldElement(
-                      labelText: "Origem",
-                      showSearchIcon: true,
-                      haveController: true,
-                      controller: _controllerOrigem,
-                      onChanged: (_) => _filterRides(),
-                    ).build(context),
-                    SizedBox(height: 7),
-                    TextFieldElement(
-                      labelText: "Destino",
-                      showSearchIcon: true,
-                      haveController: true,
-                      controller: _controllerDestino,
-                      onChanged: (_) => _filterRides(),
-                    ).build(context),
-                  ],
-                ),
-              ...snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
-
-                final List<dynamic> members = data['members'] ?? [];
-
-                final int vagasDisponiveis = 3 - members.length;
-
-                // Verifica se a carona foi criada pelo usuário atual
-                if (data['origem'].toLowerCase().contains(
-                      _controllerOrigem.text.toLowerCase(),
-                    ) &&
-                    data['destino'].toLowerCase().contains(
-                      _controllerDestino.text.toLowerCase(),
-                    ) &&
-                    vagasDisponiveis > 0) {
-                  bool isOwner = data['creatorId'] == user.uid;
-                  return UberCard(
-                    data: data,
-                  ).build(isOwner, user, document.id, context);
-                } else {
-                  return Container();
-                }
-              }).toList(),
-            ],
-          );
-        },
+      body: ListView(
+        children: [
+          if (mostrarFiltros)
+            Column(
+              children: [
+                SizedBox(height: 5),
+                TextFieldElement(
+                  labelText: "Origem",
+                  showSearchIcon: true,
+                  haveController: true,
+                  controller: _controllerOrigem,
+                  onChanged: (_) => _filterRides(),
+                ).build(context),
+                SizedBox(height: 7),
+                TextFieldElement(
+                  labelText: "Destino",
+                  showSearchIcon: true,
+                  haveController: true,
+                  controller: _controllerDestino,
+                  onChanged: (_) => _filterRides(),
+                ).build(context),
+              ],
+            ),
+          StreamBuilder<QuerySnapshot>(
+            stream:
+                uberGroups.orderBy('updatedAt', descending: true).snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                return const Center(
+                  child: Text(
+                    "Nenhum grupo Uber encontrado.",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                );
+              }
+              return Column(
+                children:
+                    snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data() as Map<String, dynamic>;
+                      // Verifica se a carona foi criada pelo usuário atual
+                      if (data['origem'].toLowerCase().contains(
+                            _controllerOrigem.text.toLowerCase(),
+                          ) &&
+                          data['destino'].toLowerCase().contains(
+                            _controllerDestino.text.toLowerCase(),
+                          )) {
+                        bool isOwner = data['creatorId'] == user.uid;
+                        return UberCard(
+                          data: data,
+                        ).build(isOwner, user, document.id, context);
+                      } else {
+                        return Container();
+                      }
+                    }).toList(),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
